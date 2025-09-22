a186cf070b7a53cc7cc80692381d1ea2a606f631
Alex Dou <38291962+anhtudo97@users.noreply.github.com>
2025-09-22 23:57:49 +0700

    chore: update session

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -4,11 +4,13 @@ import { Client, Account, Databases, Users } from "node-appwrite";
 import { cookies } from "next/headers";
 
 export async function createSessionClient() {
+  const cookieStore = await cookies();
+
   const client = new Client()
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
 
-  const session = cookies().get("appwrite-session");
+  const session = cookieStore.get("appwrite-session");
 
   if (!session || !session.value) {
     throw new Error("No session");
